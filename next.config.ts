b39f405005465c4759dd4727f03acdff0e99d574
francismteixeira <francismteixeira@gmail.com>
2025-08-04 23:27:41 -0300

    nnn

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
- 
+   output: 'export',
+  trailingSlash: true,
 };
 
 export default nextConfig;
+module.exports = nextConfig;

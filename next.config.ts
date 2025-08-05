dd9b8d0ee4ee06738376b8b542c16e14225212f7
Vinicius Tengaten <vinitengaten@gmail.com>
2025-08-05 13:47:55 -0300

    add cloudflare support

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   output: 'export',
-  trailingSlash: true,
 };
 
 export default nextConfig;
-module.exports = nextConfig;
+
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
